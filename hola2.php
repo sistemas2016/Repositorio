fb92c3954ed63cbfe7a08925b2406f627ae6db14
sistemas2016 <EquipoSantiago>
2016-06-01 20:37:43 -0600

    son dos nuevos archivos
    bueno ya lo dije

---
+++ hola2.php
@@ -0,0 +1,13 @@
+<?php 
+	$ola = "Hola mundo XD";
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php echo $ola; ?>
+</body>
+</html>
